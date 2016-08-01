fdccc54dcc2693d7fffc1a3cc50d8f7471f57caa
zkangHUST <1473345713@qq.com>
2016-08-01 12:52:14 +0800

    Counting Ones

--- 1049.c
+++ 1049.c
@@ -7,56 +7,32 @@ int main()
 {
 	int n,i=0;
 	scanf("%d",&n);
-	int sum = count_1(n);
+	int sum = count(n);
 	printf("%d",sum);
 	return 0;
 }
-/*int count_0(int n)
+int count(int n)
 {
-	if(n==0)
-		return 0;
-	else if(n<10)
-		return 1;
-	char c[11]={0};
-	int m =n,i=0;
-	while(n!=0)
-        {
-                c[i]=n%10+'0';
-                i++;
-                n=n/10;
-        }
-        int len = strlen(c);
-        int tmp =0,sum=0;
-        for(i=1;i<len;i++)
-        {
-                tmp = tmp +pow(10,i-1);
-                sum = sum + tmp;
-        }
-        sum = sum +(m%((int)pow(10,len-1)))+count(m/m%((int)pow(10,len-1)));
-	return sum;
-}*/
-int count_1(int n)
-{
-	if(n==0)
-		return 0;
-	else if(n<=9)
-		return 1;
-	char c[11]={0};
-        int m =n,i=0;
-        while(n!=0)
-        {
-                c[i]=n%10+'0';
-                i++;
-                n=n/10;
-        }
-        int len = strlen(c);
-        int tmp =0,sum=0;
-        for(i=1;i<len;i++)
-        {
-                tmp = tmp +pow(10,i-1);
-                sum = sum + tmp;
-        }
-	int x= m%((int)pow(10,len-1));
-	sum = sum +x+1+count_1(x);
+	int factor = 1;
+	int sum = 0,lower,high,cur;
+	while(n/factor!=0)
+	{
+		lower = n%(factor);
+		high = n/(factor*10);
+		cur = (n/factor)%10;
+		switch(cur)
+		{
+		case 0:
+			sum += high *factor;
+			break;
+		case 1:
+			sum += high *factor + lower+1;
+			break;
+		default:
+			sum += (high+1)*factor;
+		break;
+		}
+		factor=factor*10;
+	}
 	return sum;
 }
